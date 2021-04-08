cc188cac2ba27537c316513eaa35f6b647b668a2
hagar Elgarh <hagar.elgarh98gmail.com>
2021-04-08 23:38:29 +0400

    fgh

--- src/app/Components/login/thirdincurance/thirdincurance.component.ts
+++ src/app/Components/login/thirdincurance/thirdincurance.component.ts
@@ -19,11 +19,19 @@ export class ThirdincuranceComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
+  forthFormGroup: FormGroup;
   isEditable = false;
   dateOfBirth: string=''; 
+  noyear: string=''; 
   icp1:string='';
   price:number=0;
 
+  price1:number=0;
+  price2:number=0;
+  price3:number=0;
+  price4:number=0;
+
+  yearof:number=0;
   
   cardsBool:boolean=true
 
@@ -31,6 +39,7 @@ export class ThirdincuranceComponent implements OnInit {
 listadd: Form2 = {Section:''}
 listaddthird: Secondform = {}
 listaddfirst: Firstform = {}
+listaddforth: Forthform = {}
 
 
 today: number = Date.now();
@@ -45,7 +54,7 @@ random:number= Math.floor((Math.random() * 100000000) + 1);
     });
     this.secondFormGroup = this._formBuilder.group({
    
-      id:new FormControl('احمد', Validators.required),
+      id:new FormControl('vbfdd', Validators.required),
         Booked:new FormControl('احمد', Validators.required),
         Date:new FormControl('احمد', Validators.required)
         ,Image:new FormControl('احمد', Validators.required)
@@ -53,7 +62,7 @@ random:number= Math.floor((Math.random() * 100000000) + 1);
         ,Price:new FormControl('احمد', Validators.required)
         ,Rate:new FormControl('احمد', Validators.required)
         ,Review:new FormControl('احمد', Validators.required)
-        ,Section:new FormControl('احمد', Validators.required)
+        ,Section:new FormControl('50000', Validators.required)
         ,Title:new FormControl('احمد', Validators.required)
         ,TourDiscount:new FormControl('احمد', Validators.required)
   
@@ -64,6 +73,12 @@ random:number= Math.floor((Math.random() * 100000000) + 1);
         Email: new FormControl('d@gd', [Validators.required,Validators.email]),
         Name: new FormControl('يياحمد', [Validators.required,Validators.minLength(5)]),
         Mobile: new FormControl('123456789123', Validators.required),
+      });
+
+      this.forthFormGroup = this._formBuilder.group({
+       
+        calender: new FormControl('123456789123', Validators.required),
+        date: new FormControl('123456789123', Validators.required)
       });
     }
 
@@ -86,41 +101,43 @@ random:number= Math.floor((Math.random() * 100000000) + 1);
     console.log(this.secondFormGroup.value)
     this.listadd=this.secondFormGroup.value
 
-    this.Cards=[
-      {
-      title:"شركة الانماء طوكيو",
-      NoDocument:this.price*0.05,
-      Icon:"../../../../assets/ali.jpg",
-      ColorBck:"card-header-warning"
-  },
-  {
-    title:"شركة المجموعة المتحدة",
-    NoDocument:this.price*0.04,
-    Icon:"../../../../assets/612770365.jpg",
-    ColorBck:"card-header-success"
-  },
-  {
-    title:"شركة المتوسط والخليج",
-    NoDocument:this.price*0.02,
-    Icon:"../../../../assets/mid.png",
-    ColorBck:"card-header-danger"
-  },
-  {
-    title:"الدرع العربي للتأمين",
-    NoDocument:this.price*0.1,
-    Icon:"../../../../assets/dre.png",
-    ColorBck:"card-header-info"
-  },
- 
-  
-  ];
   
   }
 
   thirdform(){
 
     console.log(this.dateOfBirth)
+    this.datee()
+    this.Cards=[
+      {
+      title:"شركة الانماء طوكيو",
+      NoDocument:this.price1,
+      Icon:"../../../../assets/ali.jpg",
+      ColorBck:"card-header-warning"
+  },
+  {
+    title:"شركة المجموعة المتحدة",
+    NoDocument:this.price2,
+    Icon:"../../../../assets/612770365.jpg",
+    ColorBck:"card-header-success"
+  },
+  {
+    title:"شركة المتوسط والخليج",
+    NoDocument:this.price3,
+    Icon:"../../../../assets/mid.png",
+    ColorBck:"card-header-danger"
+  },
+  {
+    title:"الدرع العربي للتأمين",
+    NoDocument:this.price4,
+    Icon:"../../../../assets/dre.png",
+    ColorBck:"card-header-info"
+  },
  
+  
+  ];
+
+  
   }
 
   forthform(){
@@ -173,7 +190,7 @@ this.item.elmohafza=this.listaddthird.Mobile
 this.item.date=this.dateOfBirth
 this.item.currentdate=this.today.toString()
 
-this.item.insurancetype="تأمين ضد الغير"
+this.item.insurancetype="تأمين شامل"
 console.log(this.item)
 
   this.insSer.addInsurance(this.item)
@@ -181,4 +198,190 @@ console.log(this.item)
 printPage() {
   window.print()
 }
+showAge:number=0
+showAge2:number=0
+
+datee(){
+  this.listaddforth=this.forthFormGroup.value
+  
+
+
+  const convertAge = new Date(this.dateOfBirth);
+  const timeDiff = Math.abs(convertAge.getTime());
+
+  
+  
+  this.showAge2=this.showAge
+
+  
+
+  if(this.listaddforth.calender=='سنة' ){
+    console.log('1yer')
+    
+   this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*4)));
+   this.showAge2=this.showAge
+   
+  this.price1= this.price*0.3
+   this.price2=this.price*0.3
+  this.price3=this.price*0.3
+  this.price4=this.price*0.3
+  console.log("price1",this.price1)
+  console.log("price2",this.price2)
+  console.log("price3",this.price3)
+  console.log("price4",this.price4)
+ 
+
+  }
+
+  else if(this.listaddforth.calender=='سنتين' ){
+    console.log('2yer')
+    
+    this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*4)));
+    this.showAge2=this.showAge
+     
+  this.price1= this.price*0.3
+  this.price2=this.price*0.4
+ this.price3=this.price*0.3
+ this.price4=this.price*0.4
+ console.log("price1",this.price1)
+  console.log("price2",this.price2)
+ console.log("price3",this.price3)
+ console.log("price4",this.price4)
+ 
+   }
+   else if(this.listaddforth.calender=='3 سنوات' ){
+    console.log('2yer')
+    
+    this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*4)));
+    this.showAge2=this.showAge
+     
+  this.price1= this.price*0.4
+  this.price2=this.price*0.5
+ this.price3=this.price*0.5
+ this.price4=this.price*0.6
+ console.log("price1",this.price1)
+  console.log("price2",this.price2)
+ console.log("price3",this.price3)
+ console.log("price4",this.price4)
+ 
+   }
+   else if(this.listaddforth.calender=='4 سنوات' ){
+    console.log('4yer')
+    
+    this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*4)));
+    this.showAge2=this.showAge
+    this.price1= this.price*0.2
+    this.price2=this.price*0.5
+   this.price3=this.price*0.45
+   this.price4=this.price*0.5
+  
+
+ console.log("price1",this.price1)
+  console.log("price2",this.price2)
+ console.log("price3",this.price3)
+ console.log("price4",this.price4)
+ 
+   }
+
+  
+
+  if(this.price<=50000 ){
+    console.log('5000<')
+    this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*4)));
+    this.showAge2=this.showAge
+    
+   this.price1= this.price1*0.9
+    this.price2=this.price2*0.7
+   this.price3=this.price3*0.6
+   this.price4=this.price4*0.5
+   console.log("price1",this.price1)
+  console.log("price2",this.price2)
+  console.log("price3",this.price3)
+  console.log("price4",this.price4)
+ 
+   }
+ 
+   else if(this.price>=50000 ){
+    console.log('5000>')
+     
+     this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*4)));
+     this.showAge2=this.showAge
+      
+  
+    
+  this.price1= this.price1*0.08
+  this.price2=this.price2*0.3
+ this.price3=this.price3*0.09
+ this.price4=this.price4*0.6
+    
+ console.log("price1",this.price1)
+  console.log("price2",this.price2)
+ console.log("price3",this.price3)
+ console.log("price4",this.price4)
+    }
+   else{
+    this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*2)));
+    this.showAge2=this.showAge
+ 
+   }
+
+   if(this.yearof<=2000 ){
+    console.log('5000<')
+    this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*4)));
+    this.showAge2=this.showAge
+    
+   this.price1= this.price1*0.9
+    this.price2=this.price2*0.9
+   this.price3=this.price3*0.9
+   this.price4=this.price4*0.9
+   console.log("price1",this.price1)
+  console.log("price2",this.price2)
+  console.log("price3",this.price3)
+  console.log("price4",this.price4)
+ 
+   }
+ 
+   else if(this.yearof>2000 && this.yearof<=2018 ){
+    console.log('5000>')
+     
+     this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*4)));
+     this.showAge2=this.showAge
+      
+  
+    
+     this.price1= this.price1*0.7
+     this.price2=this.price2*0.8
+    this.price3=this.price3*0.7
+    this.price4=this.price4*0.8
+    
+ console.log("price1",this.price1)
+  console.log("price2",this.price2)
+ console.log("price3",this.price3)
+ console.log("price4",this.price4)
+    }
+
+    else if(this.yearof>2000 && this.yearof<=2018 ){
+      console.log('5000>')
+       
+       this.showAge = Math.floor((timeDiff + ((1000 * 3600 * 24)*365*4)));
+       this.showAge2=this.showAge
+        
+    
+      
+       this.price1= this.price1*1.1
+       this.price2=this.price2*1.1
+      this.price3=this.price3*1.2
+      this.price4=this.price4*1.2
+      
+   console.log("price1",this.price1)
+    console.log("price2",this.price2)
+   console.log("price3",this.price3)
+   console.log("price4",this.price4)
+      }
+    }
+
+
+    resetform(gg:any){
+      gg.reset();
+    }
 }
---
+++ src/app/ViewModals/forthform.ts
@@ -0,0 +1,4 @@
+export interface Forthform {
+    calender?:string,
+    date?:string
+}
